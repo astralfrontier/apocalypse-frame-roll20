efeb6ba6a766307f65cfd5acf399c90f5264f34a
Bill Garrett <garrett@astralfrontier.org>
2023-04-15 16:02:46 -0700

    Don't overcomplicate string manipulation

--- src/autofill.ts
+++ src/autofill.ts
@@ -8,18 +8,15 @@ on('change:frame_model', (event) => {
 })
 
 on('change:system_modular_1_name change:system_modular_2_name', (event) => {
-  const [_system, _modular_, slot, _attr] = (event.sourceAttribute || '').split(
-    '_'
-  )
+  const slot = (event.sourceAttribute || '').replace(/_name$/, '')
   const system = variables.systems.find(
     (system: any) => system.slot_name == event.newValue
   )
   if (system) {
     const O: any = {}
     for (let key in system) {
-      O[`system_modular_${key.replace('slot', slot)}`] = system[key]
+      O[`${key.replace('slot', slot)}`] = system[key]
     }
-    console.dir(O)
     setAttrs(O)
   }
 })
@@ -27,7 +24,7 @@ on('change:system_modular_1_name change:system_modular_2_name', (event) => {
 on(
   'change:arm1_name change:arm2_name change:arm1b_name change:arm2b_name',
   (event) => {
-    const [slot, _attr] = (event.sourceAttribute || '').split('_')
+    const slot = (event.sourceAttribute || '').replace(/_name$/, '')
     const armament = variables.armaments.find(
       (arm: any) => arm.slot_name == event.newValue
     )
