c0ae70b0eb8a15b5e524143dcc219ae2124f0e25
Bill Garrett <garrett@astralfrontier.org>
2023-04-15 10:43:42 -0700

    #4: Add sheetworkers for updating trackers

--- src/sheetworkers.ts
+++ src/sheetworkers.ts
@@ -2,17 +2,44 @@
 
 // Documentation: https://wiki.roll20.net/Sheet_Worker_Scripts
 
+function updateTrackersFromToken(event: Roll20Event) {
+  if (event.sourceType == 'player') {
+    const attr = event.sourceAttribute || ''
+    getAttrs([attr], (v) => {
+      const newValue = parseInt(v[attr])
+      const O: any = {}
+      for (let i of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]) {
+        O[`${attr}_box_${i}`] = i > newValue ? '0' : 'on'
+      }
+      console.dir({ newValue, O })
+      setAttrs(O)
+    })
+  }
+}
+
 function recalculateTrackerValues(event: Roll20Event) {
   if (event.sourceType == 'player') {
     const [attr, _box, slot] = (event.sourceAttribute || 'foo_bar_1').split('_')
-    const newValue =
-      event.newValue == 'on' ? parseInt(slot) : parseInt(slot) - 1
-    const O: any = {}
-    for (let i of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]) {
-      O[`${attr}_box_${i}`] = i > newValue ? '0' : 'on'
-    }
-    O[attr] = newValue
-    setAttrs(O)
+    getAttrs([attr], (v) => {
+      const oldValue = parseInt(v[attr])
+      // if the checkbox of interest is "off", but current value > newValue, we really mean for it to be "on" at a lower level
+      const slotValue = parseInt(slot) || 0
+      let newValue: number = 0
+      if (event.newValue == 'on') {
+        newValue = slotValue
+      } else if (oldValue > slotValue) {
+        newValue = slotValue
+      } else {
+        newValue = slotValue - 1
+      }
+      const O: any = {}
+      for (let i of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]) {
+        O[`${attr}_box_${i}`] = i > newValue ? '0' : 'on'
+      }
+      O[attr] = newValue
+      console.dir({ newValue, O })
+      setAttrs(O)
+    })
   }
 }
 
@@ -31,6 +58,8 @@ function recalculateTrackerMaximums(event: Roll20Event) {
     }
   )
 }
+
+on('change:vigor change:tension change:fuel', updateTrackersFromToken)
 
 for (let i of [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]) {
   on(
