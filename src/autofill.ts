1c2c828aebe3696cb8aff5f41924fba117b24b6c
Binary Star <binary.star.games@gmail.com>
2023-09-23 02:27:38 -0400

    Lots of stylistic tweaks.

--- src/autofill.ts
+++ src/autofill.ts
@@ -5,6 +5,34 @@ on('change:frame_model', (event) => {
   if (frame) {
     setAttrs(frame)
   }
+  getAttrs(
+    [
+      'attr_arm1_damage',
+      'attr_arm2_damage',
+      'attr_arm1b_damage',
+      'attr_arm2b_damage',
+      'attr_system_signature_damage',
+      'attr_system_modular_1_damage',
+      'attr_system_modular_2_damage',
+    ],
+    (v) => {
+      let defaults: AttributeBundle = {
+        arm1_damage: '0',
+        arm1b_damage: '3',
+        arm2_damage: '0',
+        arm2b_damage: '3',
+        system_signature_damage: '0',
+        system_modular_1_damage: '0',
+        system_modular_2_damage: '0',
+      }
+      for (let key of Object.keys(defaults)) {
+        if (v[key]) {
+          delete defaults[key]
+        }
+      }
+      setAttrs(defaults)
+    }
+  )
 })
 
 on('change:system_modular_1_name change:system_modular_2_name', (event) => {
